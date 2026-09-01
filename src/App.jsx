1a754356ea484a038b5067f301e4bce242982d19
madortill <mador.till@gmail.com>
2026-09-01 16:17:42 +0300

    01/09/26 finish #2?

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import OpeningPage from "./pages/OpeningPage/OpeningPage";
 import LearningPage from "./pages/LearningPage/LearningPage";
 import EndPage from "./pages/EndPage/EndPage";
 
+import { preloadAssets } from "./utils/preloadAssets";
+
 import "./css/App.css";
 
 function App() {
+  const [assetsLoaded, setAssetsLoaded] = useState(false);
+
+  useEffect(() => {
+    preloadAssets().then(() => {
+      setAssetsLoaded(true);
+    });
+  }, []);
+
+  if (!assetsLoaded) {
+    return (
+      <div className="app-loading">
+        <div className="app-loading-spinner" />
+
+        <p>טוענים את הלומדה...</p>
+      </div>
+    );
+  }
+
   return (
     <div className="app">
       <Routes>
-
-        {/* פתיחת הלומדה */}
         <Route
           path="/"
           element={<OpeningPage />}
         />
 
-        {/* הלומדה עצמה */}
         <Route
           path="/learning"
           element={<LearningPage />}
         />
 
-        {/* סיום */}
         <Route
           path="/end"
           element={<EndPage />}
         />
-
       </Routes>
     </div>
   );
